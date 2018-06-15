86aea1fe2e99386c2e4c8736ee07fd8cca4a319c
SamSteele01 <ssteele017@gmail.com>
2018-06-14 22:28:05 -0400

    Add comments

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,3 +3,5 @@ var BookLocal = artifacts.require('./BookLocal.sol');
 module.exports = function(deployer,networks,accounts) {
   deployer.deploy(BookLocal,[accounts[0]],accounts[0]);
 };
+
+// need to deploy MultiDigWallet and Escrow...
